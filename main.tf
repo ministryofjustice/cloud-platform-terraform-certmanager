295968aaed5bdfa349cf9a512292a127334b4774
Sablu Miah <sablu.miah@digital.justice.gov.uk>
2020-10-23 13:47:35 +0100

    remove blank line

--- main.tf
+++ main.tf
@@ -14,7 +14,6 @@ resource "kubernetes_namespace" "cert_manager" {
       "cloud-platform.justice.gov.uk/environment-name" = "production"
       "cloud-platform.justice.gov.uk/is-production"    = "true"
       "certmanager.k8s.io/disable-validation"          = "true"
-      
     }
 
     annotations = {
