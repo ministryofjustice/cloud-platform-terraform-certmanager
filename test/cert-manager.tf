4633a65cc2a9ab29844999ec568c924047a0ce84
Razvan Cosma <razvan.cosma@digital.justice.gov.uk>
2021-07-29 12:45:34 +0100

    fix test

--- test/cert-manager.tf
+++ test/cert-manager.tf
@@ -4,7 +4,7 @@ module "cert_manager" {
 
   iam_role_nodes      = "arn:aws:iam::000000000000:node"
   cluster_domain_name = "cert-manager.cloud-platform.service.justice.gov.uk"
-  hostzone            = "AAATEST"
+  hostzone            = ["AAATEST"]
 
   dependence_prometheus = "ignore"
   dependence_opa        = "ignore"
