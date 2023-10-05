b824b9544bdd09af95348eb438acd911a7141feb
Jack Stockley <jack.stockley@digital.justice.gov.uk>
2023-10-05 17:05:15 +0100

    feat(TF): cert-manager 1.7.2 > 1.13.1 upgrade

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ resource "helm_release" "cert_manager" {
   chart         = "cert-manager"
   repository    = "https://charts.jetstack.io"
   namespace     = kubernetes_namespace.cert_manager.id
-  version       = "v1.7.2"
+  version       = "v1.13.1"
   recreate_pods = true
 
   values = [templatefile("${path.module}/templates/values.yaml.tpl", {
