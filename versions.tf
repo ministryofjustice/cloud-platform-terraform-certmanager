10d4ddc0ebbadc06e382510b2aa79c5f898df3d8
Jake Mulley <jakemulley@users.noreply.github.com>
2024-01-03 09:39:25 +0000

    Fix hashicorp/helm version pinning constraints
    This version constraint (`~>`) currently causes an issue when upgrading the `hashicorp/helm` provider in cloud-platform-infrastructure (see [build](https://concourse.cloud-platform.service.justice.gov.uk/builds/3262451)).

--- versions.tf
+++ versions.tf
@@ -5,7 +5,7 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.6.0"
+      version = ">= 2.6.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
